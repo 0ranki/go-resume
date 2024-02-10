ee283edfd98860b7ccf78126421f7ef31a440516
Jarno Rankinen <jarno@oranki.net>
2024-02-10 17:56:37 +0200

    Print button, printing layout improvements. Summary is full width.

--- strings.go
+++ strings.go
@@ -14,6 +14,7 @@ func loadStrings() {
 		"see_dark":            "See Dark Theme",
 		"see_light":           "See Light Theme",
 		"all_rights_reserved": "All Rights Reserved.",
+		"print_dark":          "For better results, please print the page using the light theme",
 		"powered_by":          "Powered by <a href=\"https://github.com/0ranki/go-resume\">go-resume</a>",
 	}
 
@@ -29,6 +30,7 @@ func loadStrings() {
 		"see_dark":            "Tumma teema",
 		"see_light":           "Vaalea teema",
 		"all_rights_reserved": "Kaikki oikeudet pidätetään.",
+		"print_dark":          "Paremman tuloksen varmistamiseksi tulosta sivu käyttäen vaaleaa teemaa",
 		"powered_by":          "Luotu käyttäen sovellusta <a href=\"https://github.com/0ranki/go-resume\">go-resume</a>",
 	}
 }
