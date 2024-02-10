5a19ed18e75ccb26f8d3c33872e8def05d35591e
Jarno Rankinen <jarno@oranki.net>
2024-02-10 10:56:04 +0200

    Add option to disable Github Corner, update resume.example.yaml

--- resume.go
+++ resume.go
@@ -103,12 +103,13 @@ type Social struct {
 type Socials []Social
 
 type Meta struct {
-	Language    string `yaml:"language"`
-	Author      string `yaml:"author"`
-	Title       string `yaml:"title"`
-	Description string `yaml:"description"`
-	Robots      string `yaml:"robots"`
-	ThemeColor  string `yaml:"theme-color"`
+	Language     string `yaml:"language"`
+	Author       string `yaml:"author"`
+	Title        string `yaml:"title"`
+	Description  string `yaml:"description"`
+	Robots       string `yaml:"robots"`
+	ThemeColor   string `yaml:"theme-color"`
+	GithubCorner bool   `yaml:"github-corner"`
 }
 
 type Language struct {
